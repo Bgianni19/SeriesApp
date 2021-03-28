51eeaa21e3b5d1224de00ff2df17bbf1d323c19c
Gianni Barakat <barakatgianni@gmail.com>
2021-03-28 14:09:31 +0300

    App 0.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import "./App.css";
+import { ThemeProvider, Switch, Container } from "@material-ui/core";
+import AllBookCards from "./components/BookCards/AllBookCards";
+import defaultTheme from "./themes/defaultTheme";
+import darkTheme from "./themes/darkTheme";
 
 function App() {
-  return <div></div>;
+  const [Theme, setTheme] = useState(defaultTheme);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <Switch
+        onClick={() =>
+          Theme === defaultTheme ? setTheme(darkTheme) : setTheme(defaultTheme)
+        }
+      />
+      <Container>
+        <AllBookCards />
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;

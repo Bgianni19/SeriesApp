144cbb601b7d202e7c619233af00a6a4f8b3812c
Gianni Barakat <barakatgianni@gmail.com>
2021-04-02 19:06:09 +0300

    App 0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import "./App.css";
-import { ThemeProvider, Switch, Container } from "@material-ui/core";
-import AllBookCards from "./components/BookCards/AllBookCards";
+import { ThemeProvider, Switch } from "@material-ui/core";
 import defaultTheme from "./themes/defaultTheme";
 import darkTheme from "./themes/darkTheme";
+import NavBar from "./components/NavBar/NavBar";
+import Search from "./components/SearchBox/Search";
+import AllBookCards from "./components/BookCards/AllBookCards";
 
 function App() {
-  const [Theme, setTheme] = useState(defaultTheme);
+  const [theme, setTheme] = useState(defaultTheme);
+  const [searchInput, setSearchInput] = useState("");
 
   return (
-    <ThemeProvider theme={Theme}>
-      <Switch
-        onClick={() =>
-          Theme === defaultTheme ? setTheme(darkTheme) : setTheme(defaultTheme)
-        }
-      />
-      <Container>
-        <AllBookCards />
-      </Container>
+    <ThemeProvider theme={theme}>
+      <NavBar />
+      <Search handleInput={(Input) => setSearchInput(Input)}>
+        <Switch
+          color="primary"
+          onClick={(event) =>
+            event.target.checked ? setTheme(darkTheme) : setTheme(defaultTheme)
+          }
+        />
+      </Search>
+      <AllBookCards handleSearch={searchInput} />
     </ThemeProvider>
   );
 }

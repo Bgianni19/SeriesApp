14e06c3e66acec84e8cb68bf551b0ffd22f32db1
Gianni Barakat <barakatgianni@gmail.com>
2021-04-04 15:35:04 +0300

    App 0.2

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           }
         />
       </Search>
-      <AllSeriesCards handleSearch={searchInput} />
+      <AllSeriesCards input={searchInput} />
     </ThemeProvider>
   );
 }
